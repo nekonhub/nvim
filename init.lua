e593a5f13b597079ef10194d67be6c264d313d50
Neil Connolly <neil@dequ.co.uk>
2025-05-12 12:42:20 +0100

    basic file structure set up

---
+++ init.lua
@@ -0,0 +1,20 @@
+-- INIT.LUA is the starting point for my Neovim config files
+-- 2025-05-12 config, using Neovim r0.11.1 in Fedora 42
+-- An exercise in simplification
+-- Function > Form
+-- Native > Plugin
+-- I don't like the UserName folder common in many nvim configs
+-- Uses ".lazy" (DOT LAZY) to remove the eponymous folder
+
+-- When switching to Lua based config, the best advice I can offer is "RTFM"
+-- https://neovim.io/doc/user/index.html
+-- https://neovim.io/doc/user/lua.html
+-- ":h" is your friend
+
+-- Keymaps and Options loaded first to ensure leader-key is set early
+require("settings.keymaps")
+require("settings.options")
+
+-- Require the Lazy.lua file to load plugins
+require(".lazy")
+
