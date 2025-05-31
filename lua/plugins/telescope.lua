908a4d8a2f51ab70c150d478d3d8976024cb6e50
Neil Connolly <neil@dequ.co.uk>
2025-05-31 16:56:36 +0100

    Telescope to find and switch files

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,43 @@
+-- Telescope for finding files
+
+-- [TODO} changes to fix swap to Treesitter "main" branch
+-- Curnetly no highlightiing in Telescope preview window
+
+return {
+    "nvim-telescope/telescope.nvim",
+    tag = '0.1.8',
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    },
+    config = function()
+        local actions = require("telescope.actions")
+        require('telescope').setup {
+            defaults = {
+                path_display = { "truncate" },
+                mappings = {
+                    i = {
+                        ["<C-k>"] = actions.move_selection_previous, -- move to prev result
+                        ["<C-j>"] = actions.move_selection_next,     -- move to next result
+                        ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+                    },
+                },
+            },
+            extensions = {
+                fzf = {}
+            },
+        }
+
+        require('telescope').load_extension('fzf')
+
+        -- set keymaps
+        local keymap = vim.keymap -- for conciseness
+
+        keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+        keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Fuzzy find open buffers" })
+        keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+        keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+        keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+        keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { desc = "Find help grepping topics" })
+    end
+}
