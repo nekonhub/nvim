7fa7c3dab2d3ccdef4bdc346890653c884a6bf1f
Neil Connolly <neil@dequ.co.uk>
2025-05-31 14:16:12 +0100

    Autoformat on write added

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -44,13 +44,30 @@ return {
             },
         }
 
-    },  -- END of dependencies
+    }, -- END of dependencies
 
     -- Configure LSPCONFIG
     config = function()
         -- MASON-LSPCONFIG handles auto attatch and config of individual LSPs
         -- Uses default LSPCONFIG settings
 
+        --CAPABILITIES
+        local capabilities = require('blink.cmp').get_lsp_capabilities()
+        require("lspconfig").lua_ls.setup { capabilites = capabilities }
+
+        -- Auto format on "write"
+        vim.api.nvim_create_autocmd('LspAttach', {
+            callback = function(args)
+                local c = vim.lsp.get_client_by_id(args.data.client_id)
+                if not c then return end
+
+                vim.api.nvim_create_autocmd('BufWritePre', {
+                    buffer = args.buf,
+                    callback = function()
+                        vim.lsp.buf.format({ bufnr = args.buf, id = c.id })
+                    end,
+                })
+            end
+        })
     end,
 }
-
