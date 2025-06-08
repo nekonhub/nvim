c6ea40a33972323ee7addfbc80d850fe452b217e
Neil Connolly <neil@dequ.co.uk>
2025-06-08 11:43:22 +0100

    Markview.lua added

---
+++ lua/plugins/markview.lua
@@ -0,0 +1,13 @@
+-- MARKVIEW.LUA
+-- -Markdown rendering in markdown files
+
+return {
+    "OXY2DEV/markview.nvim",
+    lazy = false,
+
+    -- For blink.cmp's completion
+    -- source
+    dependencies = {
+        "saghen/blink.cmp"
+    },
+};
