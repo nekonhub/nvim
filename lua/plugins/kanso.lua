320ead543128ab4de1f48afa0ee815f84664b9d3
Neil Connolly <neil@dequ.co.uk>
2026-06-09 08:20:32 +0100

    Change to comment colour - feint

--- lua/plugins/kanso.lua
+++ lua/plugins/kanso.lua
@@ -1,7 +1,5 @@
 -- KANSO.LUA - an nvim colorscheme based on Kanagawa
 -- Dark and subtle, has Kanagawa advantages without color bling.
--- I think most people see signals in code colours,
--- but I just see distraction, it dazzles me.
 
 return {
     "webhooked/kanso.nvim",
